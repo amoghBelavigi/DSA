440868e83dc366da0756b980b284b2b9ee31060a
Amogh Belavigi <75718896+amoghBelavigi@users.noreply.github.com>
2022-05-06 13:35:42 +0530

    Add files via upload

---
+++ moveZeros.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+public class Main
+{
+    public static void printArray(int arr[], int n){   //to print the array
+        System.out.println("The modified array is: ");
+        for(int i=0;i<n;i++){
+		    System.out.println(arr[i]+" ");
+		}
+		System.out.println();
+    }
+    
+    public static void moveZeros(int[] arr,int n){
+        int j=0; //focuses on zero elemets
+        for(int i=0;i<n;i++){  //will focus on non zero elemets
+            if(arr[i]!=0&&arr[j]==0){  //if i is not pointing to zero and j is pointing to a zero
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            if(arr[j]!=0){
+                j++;
+            }
+        }
+        printArray(arr,n);
+    }
+    
+    
+    
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int n=input.nextInt();
+		int arr[]= new int[n];
+		System.out.println("Enter the array elemets: ");
+		for(int i=0;i<n;i++){
+		    arr[i]=input.nextInt();
+		}
+		moveZeros(arr,n);
+	}
+}
