dcb82347102282557e4aff0128a8ed82e4c4cebe
Amogh Belavigi <75718896+amoghBelavigi@users.noreply.github.com>
2022-07-30 13:48:19 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -8,6 +8,14 @@ public class Main
         }
         System.out.println("The factorial of "+n+" is "+result);
     }
+	 
+	//other effecient method:
+	/*if(n==0 || n==1){
+             return 1;
+        }
+        else{
+             return n*factorial(n-1);
+        }*/
     
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
